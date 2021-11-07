d719953e69d157edda1821b973c8edc1ed889e78
salwa alqurashi <Salwa.Kheder.abt.js@tuwaiq.edu.sa>
2021-11-07 17:10:35 +0300

    h.w

---
+++ login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form")
+
+function login(event) {
+    event.preventDefault()
+    const email = form.elements.email.value
+    const password = form.elements.password.value
+
+    const body = {
+        email: email,
+        password: password
+    }
+
+
+    axios.post("https://vast-chamber-06347.herokuapp.com/api/user/auth", body)
+        .then(response => {
+            const token = response.data
+            console.log(token)
+            localStorage.token = token
+
+            window.location.href = "index.html"
+
+        })
+}
+function logout() {
+    localStorage.removeItem("token")
+}
+
